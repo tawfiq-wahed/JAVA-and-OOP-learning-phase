c2a7fd61300da07c48126381c07eca356561b9e3
Tawfiq Wahid <tawfiqbinwahed@gmail.com>
2025-10-27 20:33:32 +0600

    Add tawfiq.cpp

---
+++ tawfiq.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<int> a(n);
+        for(int i=0;i<n;i++) cin>>a[i];
+        sort(a.begin(),a.end());
+        int ans=0;
+        for(int i=1;i<n;i++){
+            ans+=a[i]-a[0];
+        }
+        cout<<ans<<"\n";
+    }
+    return 0;
+}
